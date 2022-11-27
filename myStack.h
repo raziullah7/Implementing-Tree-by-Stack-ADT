cf42817086832990c7c85a47d70ec474e536f583
Razi Ullah <raziullah913@gmail.com>
2022-11-27 20:00:01 +0500

    first commit

---
+++ myStack.h
@@ -0,0 +1,23 @@
+#pragma once
+
+template <class T>
+class myStack {
+private:
+	T* s;
+	int len;
+	int size;
+	int top;
+public:
+	myStack(int cap);
+	~myStack() { delete[] s; }
+	bool IsEmpty();
+	bool IsFull();
+	void Push(T);
+	void Pop(T&);
+	int Length() { return len; }
+	T TopElement();
+	void Display();
+	int Search(T);
+	bool Find(int, T&);
+};
+
